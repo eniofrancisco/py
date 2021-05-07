17fb39f23b6171d3104f768b1f425c6f867478aa
eniofrancisco <83376618+eniofrancisco@users.noreply.github.com>
2021-05-07 02:29:06 -0300

    Add files via upload

---
+++ src/req.py
@@ -0,0 +1,57 @@
+import requests
+import json
+
+from flask import Flask, request, Response
+from flask_restful import Resource, Api
+from json import dumps
+
+app = Flask(__name__)
+api = Api(app)
+
+def lerArquivo(tipo, teste):
+	try:
+		if tipo == "certificado":
+			if teste == "1A2E3A4E5B6D7B8E9B10A":
+				f = open("files/%s_%s.htm" % (tipo, teste), "r")
+			else:
+				f = open("files/reprovado_001.htm")
+		elif teste == "":
+			f = open("files/%s_200.htm" % (tipo), "r")
+		else:
+			f = open("files/%s_%s.htm" % (tipo, teste), "r")
+	except Exception:
+		f = open("files/html_404.htm", "r")
+
+	return f.read()
+
+def tipoContent(tipo):
+	if tipo == "json":
+		return "application/json"
+	elif tipo == "html":
+		return "text/html"
+	elif tipo == "xml":
+		return "text/xml"
+	else:
+		return "text/html"
+
+# Define Classes
+class Simulador(Resource):
+	def get(self, arg1, arg2):
+		return Response(response=lerArquivo(arg1, arg2),content_type=tipoContent(arg1),status=200)
+	def post(self, arg1, arg2):
+		return Response(response=lerArquivo(arg1, arg2),content_type=tipoContent(arg1),status=200)
+# Add Routes
+api.add_resource(Simulador, '/<arg1>/<arg2>')
+
+# Define Classes
+class SimuladorTipo(Resource):
+	def get(self, arg1):
+		return Response(response=lerArquivo(arg1, ""),content_type=tipoContent(arg1),status=200)
+	def post(self, arg1):
+		return Response(response=lerArquivo(arg1, ""),content_type=tipoContent(arg1),status=200)
+# Add Routes
+api.add_resource(SimuladorTipo, '/<arg1>')
+
+# Main
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port='5002')
